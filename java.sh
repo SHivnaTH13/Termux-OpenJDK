1d47403990678f4b45388b7aa8b23a78f2dcba15
SHIVNATH SHIRSAT <50328373+SHivnaTH13@users.noreply.github.com>
2022-01-10 00:03:38 +0530

    Update java.sh

--- java.sh
+++ java.sh
@@ -1,17 +1,23 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
+apt update && apt install wget -y
+
 #Checking for existing Java installation
 
 if [ -e $PREFIX/bin/javac ]
 then
-	echo -e "\e[32mJava is already installed!"
-	echo
-	exit
+  echo -e "\e[32mJava is already installed!"
+  echo
+  exit
 else
+  if [ -e $PREFIX/bin/dx ]
+  then
+    apt remove dx
+  fi
 
 #Actual installation
 
-	wget https://github.com/SHivnaTH13/Termux-OpenJDK/releases/download/v8-232/openjdk-8-jdk_8u232.deb --show-progress -q
-	dpkg -i openjdk-8-jdk_8u232.deb
-	rm openjdk-8-jdk_8u232.deb
+  wget https://github.com/SHivnaTH13/Termux-OpenJDK/releases/download/v8-311/openjdk-8-jdk_8u311_all.deb --show-progress -q
+  dpkg -i openjdk-8-jdk_8u311_all.deb
+  rm openjdk-8-jdk_8u311_all.deb
 fi
