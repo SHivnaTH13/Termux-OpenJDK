6808cb288d2c8e8127ca322c69f5699c72d8c16c
SHIVNATH SHIRSAT <50328373+SHivnaTH13@users.noreply.github.com>
2022-04-26 20:32:08 +0530

    Update java.sh

--- java.sh
+++ java.sh
@@ -1,6 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-apt update && apt install wget -y
+apt update && apt upgrade -y && apt install wget -y
 
 #Checking for existing Java installation
 
@@ -12,12 +12,17 @@ then
 else
   if [ -e $PREFIX/bin/dx ]
   then
-    apt remove dx -y
+    apt-get remove dx -y
+  fi
+  
+  if [ -e $PREFIX/bin/ecj ]
+  then
+    apt-get remove ecj -y
   fi
 
 #Actual installation
 
-  wget https://github.com/SHivnaTH13/Termux-OpenJDK/releases/download/v8-311/openjdk-8-jdk_8u311_all.deb --show-progress -q
-  dpkg -i openjdk-8-jdk_8u311_all.deb
-  rm openjdk-8-jdk_8u311_all.deb
+  wget https://github.com/SHivnaTH13/Termux-OpenJDK/releases/download/v8-331/openjdk-8-jdk_8u331_all.deb --show-progress -q
+  dpkg -i openjdk-8-jdk_8u331_all.deb
+  rm openjdk-8-jdk_8u331_all.deb
 fi
